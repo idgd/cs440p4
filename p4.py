44b3750a064d6431f3cd33c6c29e70bb67ea836f
idgd <isaac.dudney@icloud.com>
2018-04-10 17:02:01 -0600

    brought FIFO closer in line with requirements, ensured function purity

--- p4.py
+++ p4.py
@@ -19,36 +19,40 @@ choices = list(al)[:u]
 # generate pattern
 pattern = [choice(choices) for f in range(p)]
 # ensure all choices occur at least once
-for f in choices:
-	if f not in pattern: pattern[randint(0,len(pattern))] = f
+for f in pattern:
+	for g in choices:
+		if g not in pattern: pattern[randint(0,len(pattern) - 1)] = f
 
 # initialize slots
 slots = [None for f in range(s)]
 
-def FIFO(p,s):
+def FIFO(p,slots):
 	# slot index to replace
 	i = 0
 
+	# duplicate slots to ensure function purity
+	s = slots[:]
+
 	# return struct
-	t = [FIFO.__name__ + "  " + str(f) + ": " for f in range(len(s) + 1)]
-	t[0] = "Ref Str: " + "".join(p)
+	r = [FIFO.__name__ + "  " + str(f) + ": " for f in range(len(s) + 1)]
+	r[0] = "Ref Str: " + " ".join(p)
 
 	# for each item in pattern
 	for f in p:
 		# if item exists in slots, write cache hit
 		if f in s:
 
-			for g in range(1,len(t)):
-				if g - 1 == s.index(f): t[g] += "*"
-				else: t[g] += " "
+			for g in range(1,len(r)):
+				if g - 1 == s.index(f): r[g] += "* "
+				else: r[g] += "  "
 
 		# if empty slot exists
 		elif None in s:
 
 			# write cache miss
-			for g in range(1,len(t)):
-				if g - 1 == s.index(None): t[g] += f
-				else: t[g] += " "
+			for g in range(1,len(r)):
+				if g - 1 == s.index(None): r[g] += f + " "
+				else: r[g] += "  "
 
 			# set first empty slot to item
 			s[s.index(None)] = f
@@ -57,9 +61,9 @@ def FIFO(p,s):
 		else:
 
 			# write cache miss
-			for g in range(1,len(t)):
-				if g - 1 == i: t[g] += f
-				else: t[g] += " "
+			for g in range(1,len(r)):
+				if g - 1 == i: r[g] += f + " "
+				else: r[g] += "  "
 
 			# replace slot at index
 			s[i] = f
@@ -70,7 +74,10 @@ def FIFO(p,s):
 			else:
 				i = 0
 
-	return(t)
+	return(r)
+
+def LRU(p,s):
+	return()
 
 def pr(s):
 	for f in s:
